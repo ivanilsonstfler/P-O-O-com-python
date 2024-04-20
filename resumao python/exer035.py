aceb9ecba9017db4b5fceb8aa78f64b4485f4e97
ivailson <ivanilson57637@estudante.ifb.edu.br>
2024-04-19 22:07:11 -0300

    cont

---
+++ resumao python/exer035.py
@@ -0,0 +1,10 @@
+# 34. Reescreva o exercício anterior uƟlizando a estrutura ENQUANTO. 
+
+valor1 = float(input("Digite o primeiro valor: "))
+valor2 = 0
+
+while valor2 == 0:
+    valor2 = float(input("Digite o segundo valor (não pode ser zero): "))
+
+print("O resultado da divisão é: ", valor1 / valor2)
+
